c883b12e62b31d9fff4ea299f974754b37f68b83
sujp <sujp@sino-bridge.com>
2018-05-28 16:23:16 +0800

    修改了最新合并发现的问题

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@
   //
   // To change configuration for local development, copy this file to
   // app/config.local.js and edit the copy.
-  var masterPublicHostname = '192.168.153.137:8443';
+  var masterPublicHostname = 'localhost:8443';
   var devConsoleHostname = 'localhost:9000';
   window.OPENSHIFT_CONFIG = {
     apis: {
